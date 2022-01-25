dd3ac567b8c5b3d4ebce57d18db76d1f0b406d13
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2022-01-25 19:54:41 -0300

    [dev] Added js reduce example of grouping objects by a key (property).

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,5 +1,5 @@
 const fetchUsers = fetch('data/users.json');
-const fetchUsers = fetch('data/colors.json');
+const fetchColors = fetch('data/colors.json');
 
 Promise.all([fetchUsers, fetchColors]).then(value => {
   return Promise.all(values.map(value => value.json()));
